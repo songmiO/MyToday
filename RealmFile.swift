e92f53ea90eeb529e373d5e3ebd98add69886446
songmi oh <dhthd1060@gmail.com>
2021-06-16 22:10:59 +0900

    My today 앱 구현(미완성본)

---
+++ RealmFile.swift
@@ -0,0 +1,8 @@
+//
+//  RealmFile.swift
+//  
+//
+//  Created by j on 2020/01/02.
+//
+
+import Foundation
---
+++ SongCollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SongCollectionViewController.swift
+//  myToday
+//
+//  Created by j on 2020/01/01.
+//  Copyright © 2020 Claire. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class SongCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ }
+}
